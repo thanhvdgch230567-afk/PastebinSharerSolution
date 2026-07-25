dcbdf46dc6e7f199d4555851c64adab4c7a5c6ed
unknown <thanhvdgch230567@gmail.com>
2026-07-25 10:03:31 +0700

    feat: xong 4api

---
+++ PastebinSharer/Models/DTOs/PasteResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PastebinSharer.Models.DTOs
+{
+    public class PasteResponseDto
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Language { get; set; } = "plaintext";
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+        public bool IsPrivate { get; set; }
+        public int ViewCount { get; set; }
+    }
+}
--- PastebinSharer/Program.cs
+++ PastebinSharer/Program.cs
@@ -1,15 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using PastebinSharer.Data;
+using PastebinSharer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped<PasteService>();
 
 var app = builder.Build();
 
@@ -23,5 +27,12 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//    db.Database.Migrate();
+//}
 
 app.Run();
